bdc1ed5689984a704fca25903b5de14013d96148
Matheus Ferreira Frederico <maathe.f@gmail.com>
2017-05-05 03:51:42 -0400

    Docs, for website in github

--- app/javascript/transfferApp.js
+++
@@ -1,9 +0,0 @@
-/* ****************************************************** */
-/*
-/* Angular app comunication aplication.
-/* 
-/* Var @var myVariableApp "Recebe angularModule"	
-/* Desenvolvido por Matheus Ferreira <maathe.f@gmail.com>
-/*
-/* ****************************************************** */
-var myVariableApp = angular.module('transfferApp', ['ui.router']);
